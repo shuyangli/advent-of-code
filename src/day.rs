2b24c6b3ba9da5072a4e282c25af10b10bc077ee
Shuyang Li <shuyang.li.95@gmail.com>
2023-12-02 19:01:54 -0500

    Provide default unimplemented methods on the Day trait.

--- src/day.rs
+++ src/day.rs
@@ -1,6 +1,10 @@
 use std::fmt::Display;
 
 pub trait Day {
-    fn part1(&self, input: &str) -> Result<Box<dyn Display>, &str>;
-    fn part2(&self, input: &str) -> Result<Box<dyn Display>, &str>;
+    fn part1(&self, _input: &str) -> Result<Box<dyn Display>, &str> {
+        return Err("Unimplemented!");
+    }
+    fn part2(&self, _input: &str) -> Result<Box<dyn Display>, &str> {
+        return Err("Unimplemented!");
+    }
 }
