b2fca36bee7eaf1859ae47421c091772228cc973
Shuyang Li <shuyang.li.95@gmail.com>
2024-12-08 14:27:18 -0500

    itertools.combinations

--- 2024/day8.py
+++ 2024/day8.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from typing import DefaultDict, List
 from lib import Grid, Coordinate, Direction
+from itertools import combinations
 from math import gcd
 
 def part1(grid: Grid[str]):
@@ -12,16 +13,15 @@ def part1(grid: Grid[str]):
 
     antinode_coords = set()
     for _, coords in letters_to_coords.items():
-        for i in range(len(coords)):
-            for j in range(i + 1, len(coords)):
-                # For every pair, calculate the positions
-                delta_vec = Direction(coords[i].i - coords[j].i, coords[i].j - coords[j].j)
-                coords_1 = coords[i].step(delta_vec, 1)
-                if grid.is_in_bounds(coords_1):
-                    antinode_coords.add(coords_1)
-                coords_2 = coords[j].step(delta_vec, -1)
-                if grid.is_in_bounds(coords_2):
-                    antinode_coords.add(coords_2)
+        for a, b in combinations(coords, 2):
+            # For every pair, calculate the positions
+            delta_vec = Direction(a.i - b.i, a.j - b.j)
+            coords_1 = a.step(delta_vec, 1)
+            if grid.is_in_bounds(coords_1):
+                antinode_coords.add(coords_1)
+            coords_2 = b.step(delta_vec, -1)
+            if grid.is_in_bounds(coords_2):
+                antinode_coords.add(coords_2)
     return len(antinode_coords)
 
 def part2(grid: Grid[str]):
@@ -33,30 +33,29 @@ def part2(grid: Grid[str]):
 
     antinode_coords = set()
     for _, coords in letters_to_coords.items():
-        for i in range(len(coords)):
-            for j in range(i + 1, len(coords)):
-                # For every pair, calculate the positions
-                # Reduce the delta to the smallest fraction
-                d_i = coords[i].i - coords[j].i
-                d_j = coords[i].j - coords[j].j
-                vec_gcd = gcd(d_i, d_j)
-                delta_vec = Direction(d_i // vec_gcd, d_j // vec_gcd)
+        for a, b in combinations(coords, 2):
+            # For every pair, calculate the positions
+            # Reduce the delta to the smallest fraction
+            d_i = a.i - b.i
+            d_j = a.j - b.j
+            vec_gcd = gcd(d_i, d_j)
+            delta_vec = Direction(d_i // vec_gcd, d_j // vec_gcd)
 
-                scale = 0
-                while True:
-                    new_coords = coords[i].step(delta_vec, scale)
-                    if not grid.is_in_bounds(new_coords):
-                        break
-                    antinode_coords.add(new_coords)
-                    scale += 1
+            scale = 0
+            while True:
+                new_coords = a.step(delta_vec, scale)
+                if not grid.is_in_bounds(new_coords):
+                    break
+                antinode_coords.add(new_coords)
+                scale += 1
 
-                scale = -1
-                while True:
-                    new_coords = coords[i].step(delta_vec, scale)
-                    if not grid.is_in_bounds(new_coords):
-                        break
-                    antinode_coords.add(new_coords)
-                    scale -= 1
+            scale = -1
+            while True:
+                new_coords = a.step(delta_vec, scale)
+                if not grid.is_in_bounds(new_coords):
+                    break
+                antinode_coords.add(new_coords)
+                scale -= 1
     return len(antinode_coords)
 
 def main():
