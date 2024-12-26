21d6a1880cbf9831793cc7888bb8fe0488491469
Shuyang Li <shuyang.li.95@gmail.com>
2024-12-26 18:50:33 +0100

    Day 21 Part 2

--- 2024/day21.py
+++ 2024/day21.py
@@ -1,5 +1,6 @@
-from typing import List, Dict
-from lib import Coordinate, Direction
+from typing import List, Dict, Tuple, DefaultDict
+from collections import defaultdict
+from lib import Coordinate
 
 number_keys = {
     "0": Coordinate(1, 3),
@@ -25,84 +26,104 @@ direction_keys = {
 }
 direction_gap = Coordinate(0, 0)
 
-def get_instruction_prime(instructions: List[str], keys: Dict[str, Coordinate], gap: Coordinate) -> List[str]:
-    all_possible_instructions = []
-    def process_next(idx: int, instruction: str, position: Coordinate, acc: List[str]):
-        if idx == len(instruction):
-            all_possible_instructions.append("".join(acc))
-            return
-        next_char = instruction[idx]
-        next_position = keys[next_char]
-        dx = next_position.x - position.x
-        dy = next_position.y - position.y
+transition_hypotheses: Dict[Tuple[str, str], str] = {}
 
-        if dx == 0:
-            next_acc = acc + ["^" if dy < 0 else "v"] * abs(dy) + ["A"]
-            return process_next(idx + 1, instruction, next_position, next_acc)
-        elif dy == 0:
-            next_acc = acc + ["<" if dx < 0 else ">"] * abs(dx) + ["A"]
-            return process_next(idx + 1, instruction, next_position, next_acc)
+def instruction_to_transitions(instruction: str) -> Dict[Tuple[str, str], int]:
+    transitions = defaultdict(lambda: 0)
+    current = "A"
+    for c in instruction:
+        transitions[(current, c)] += 1
+        current = c
+    return transitions
 
-        # We have a choice between left/right vs. up/down
-        # left/right first
-        current = position
-        allowed = True
-        next_acc = [] + acc
-        for _ in range(abs(dx)):
-            next_acc.append("<" if dx < 0 else ">")
-            current = current.step(Direction(-1, 0) if dx < 0 else Direction(1, 0))
-            if current == gap:
-                allowed = False
-        for _ in range(abs(dy)):
-            next_acc.append("^" if dy < 0 else "v")
-            current = current.step(Direction(0, -1) if dy < 0 else Direction(0, 1))
-            if current == gap:
-                allowed = False
-        if allowed:
-            next_acc.append("A")
-            process_next(idx + 1, instruction, next_position, next_acc)
+def get_instruction_length(transitions: DefaultDict[Tuple[str, str], int]) -> int:
+    return sum(transitions.values())
 
-        # up/down first
-        current = position
-        allowed = True
-        next_acc = [] + acc
-        for _ in range(abs(dy)):
-            next_acc.append("^" if dy < 0 else "v")
-            current = current.step(Direction(0, -1) if dy < 0 else Direction(0, 1))
-            if current == gap:
-                allowed = False
-        for _ in range(abs(dx)):
-            next_acc.append("<" if dx < 0 else ">")
-            current = current.step(Direction(-1, 0) if dx < 0 else Direction(1, 0))
-            if current == gap:
-                allowed = False
-        if allowed:
-            next_acc.append("A")
-            process_next(idx + 1, instruction, next_position, next_acc)
+# Instruction is represented as a dictionary of transition count
+def get_expanded_transitions(
+        instruction: Dict[Tuple[str, str], int],
+        keys: Dict[str, Coordinate],
+        gap: Coordinate,
+        transition_hypotheses: Dict[Tuple[str, str], str]) -> Dict[Tuple[str, str], int]:
+    out_transitions = defaultdict(lambda: 0)
+    for (current_char, next_char), count in instruction.items():
+        expanded = get_shortest_transition(current_char, next_char, keys, gap, transition_hypotheses)
+        expanded = instruction_to_transitions(expanded)
+        for (expanded_0, expanded_1), expanded_count in expanded.items():
+            out_transitions[(expanded_0, expanded_1)] += count * expanded_count
+    return out_transitions
 
-    for ins in instructions:
-        position = keys["A"]
-        process_next(0, ins, position, [])
-    return all_possible_instructions
+def get_shortest_transition(
+    current: str,
+    next: str,
+    keys: Dict[str, Coordinate],
+    gap: Coordinate,
+    transition_hypotheses: Dict[Tuple[str, str], str]) -> str:
+    if (current, next) in transition_hypotheses:
+        return transition_hypotheses[(current, next)]
+
+    current_coord = keys[current]
+    next_coord = keys[next]
+
+    dx = next_coord.x - current_coord.x
+    dy = next_coord.y - current_coord.y
+
+    if dx == 0:
+        transition_hypotheses[(current, next)] = ("^" if dy < 0 else "v") * abs(dy) + "A"
+        return transition_hypotheses[(current, next)]
+    elif dy == 0:
+        transition_hypotheses[(current, next)] = ("<" if dx < 0 else ">") * abs(dx) + "A"
+        return transition_hypotheses[(current, next)]
+
+    all_options = []
+    if not Coordinate(next_coord.x, current_coord.y) == gap:
+        all_options.append(("<" if dx < 0 else ">") * abs(dx) + ("^" if dy < 0 else "v") * abs(dy) + "A")
+    if not Coordinate(current_coord.x, next_coord.y) == gap:
+        all_options.append(("^" if dy < 0 else "v") * abs(dy) + ("<" if dx < 0 else ">") * abs(dx) + "A")
+    if len(all_options) == 1:
+        transition_hypotheses[(current, next)] = all_options[0]
+        return all_options[0]
+
+    instructions_0 = instruction_to_transitions(all_options[0])
+    instructions_1 = instruction_to_transitions(all_options[1])
+
+    while get_instruction_length(instructions_0) == get_instruction_length(instructions_1):
+        transition_hypotheses[(current, next)] = all_options[0]
+        instructions_0 = get_expanded_transitions(instructions_0, direction_keys, direction_gap, transition_hypotheses)
+        transition_hypotheses[(current, next)] = all_options[1]
+        instructions_1 = get_expanded_transitions(instructions_1, direction_keys, direction_gap,
+        transition_hypotheses)
+        if get_instruction_length(instructions_0) != get_instruction_length(instructions_1):
+            break
+    shorter = all_options[0] if get_instruction_length(instructions_0) < get_instruction_length(instructions_1) else all_options[1]
+    transition_hypotheses[(current, next)] = shorter
+    return shorter
 
 def part1(instructions: List[str]):
     total_complexity = 0
-
     for original_instruction in instructions:
-        instruction = get_instruction_prime([original_instruction], number_keys, number_gap)
-
+        transitions = instruction_to_transitions(original_instruction)
+        transitions = get_expanded_transitions(transitions, number_keys, number_gap, transition_hypotheses)
         for _ in range(2):
-            instruction_prime = get_instruction_prime(instruction, direction_keys, direction_gap)
-            instruction = instruction_prime
-        min_len = min([len(i) for i in instruction])
-        complexity = int(original_instruction[:-1]) * min_len
-        print(f"Complexity for {original_instruction}: {complexity}")
+            transitions = get_expanded_transitions(transitions, direction_keys, direction_gap, transition_hypotheses)
+        instruction_length = get_instruction_length(transitions)
+        complexity = int(original_instruction[:-1]) * instruction_length
         total_complexity += complexity
 
     return total_complexity
 
 def part2(instructions: List[str]):
-    return "Unimplemented"
+    total_complexity = 0
+    for original_instruction in instructions:
+        transitions = instruction_to_transitions(original_instruction)
+        transitions = get_expanded_transitions(transitions, number_keys, number_gap, transition_hypotheses)
+        for _ in range(25):
+            transitions = get_expanded_transitions(transitions, direction_keys, direction_gap, transition_hypotheses)
+        instruction_length = get_instruction_length(transitions)
+        complexity = int(original_instruction[:-1]) * instruction_length
+        total_complexity += complexity
+
+    return total_complexity
 
 def main():
     with open("inputs/day21") as file:
